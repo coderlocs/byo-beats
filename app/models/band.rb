5712aa04889c446924e7d34e2d3741012478b9a7
Hannah Masila <hannahmasila@gmail.com>
2023-05-03 21:53:17 -0400

    fix relationship between user and band

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,5 @@
 class Band < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, inverse_of: :bands, class_name: 'User', foreign_key: :owner_id
   include ImageUploader::Attachment(:image)
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bands, as: :owner
+  has_many :bands, inverse_of: :owner, foreign_key: :owner_id, dependent: :destroy
 
   def full_name
     "#{self.firstname} #{self.lastname}"
